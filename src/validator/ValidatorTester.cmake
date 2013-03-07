33d4d84754e48e301f8477f2356495f2703ca20b
Roman Kharitonov <refaim.vl@gmail.com>
2013-03-08 00:45:59 +1100

    Use 'diff' instead of 'cmake -E compare_files' for output and answer comparison

--- src/validator/ValidatorTester.cmake
+++ src/validator/ValidatorTester.cmake
@@ -1,15 +1,15 @@
-execute_process(COMMAND ${TESTER}
-                        ${TESTDIR}/${SUITE}.inp
-                        ${TESTDIR}/${SUITE}.fmt
-                        OUTPUT_FILE ${TESTDIR}/${SUITE}.out)
+execute_process(COMMAND ${TESTER} ${SUITE}.inp ${SUITE}.fmt
+                WORKING_DIRECTORY ${TESTDIR}
+                OUTPUT_FILE ${SUITE}.out)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files
-                        ${TESTDIR}/${SUITE}.ans
-                        ${TESTDIR}/${SUITE}.out
-                        RESULT_VARIABLE TEST_FAILED)
+execute_process(COMMAND diff -wB ${SUITE}.ans ${SUITE}.out
+                WORKING_DIRECTORY ${TESTDIR}
+                RESULT_VARIABLE DIFF_RESULT)
 
-if(${TEST_FAILED} EQUAL 1)
+if(${DIFF_RESULT} EQUAL 1)
     # CMake cannot return specified exit code from script,
     # so use FATAL_ERROR for returning non-zero code.
     message(FATAL_ERROR "Validator test '${SUITE}' is failed!")
+elseif(${DIFF_RESULT} EQUAL 2)
+    message(FATAL_ERROR "Diff trouble on test '${SUITE}'")
 endif()
