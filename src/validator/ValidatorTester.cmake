bfe3e8d4f548e64d0fcc056d7f0a5450ae825349
Roman Kharitonov <refaim.vl@gmail.com>
2013-03-09 07:47:23 -0500

    Fix tester whitespace issue
    Do not ignore line endings differences during test results
    comparison, just convert all of them to LF.
    Also use 'cmake -E compare_files' instead of 'diff'.

--- src/validator/ValidatorTester.cmake
+++ src/validator/ValidatorTester.cmake
@@ -2,14 +2,15 @@ execute_process(COMMAND ${TESTER} ${SUITE}.inp ${SUITE}.fmt
                 WORKING_DIRECTORY ${TESTDIR}
                 OUTPUT_FILE ${SUITE}.out)
 
-execute_process(COMMAND diff -wB ${SUITE}.ans ${SUITE}.out
-                WORKING_DIRECTORY ${TESTDIR}
-                RESULT_VARIABLE DIFF_RESULT)
+configure_file(${TESTDIR}/${SUITE}.ans ${TESTDIR} NEWLINE_STYLE LF)
+configure_file(${TESTDIR}/${SUITE}.out ${TESTDIR} NEWLINE_STYLE LF)
 
-if(${DIFF_RESULT} EQUAL 1)
+execute_process(COMMAND {$CMAKE_COMMAND} -E compare_files ${SUITE}.ans ${SUITE}.out
+                WORKING_DIRECTORY ${TESTDIR}
+                RESULT_VARIABLE RETURN_CODE)
+
+if(${RETURN_CODE} NOT EQUAL 0)
     # CMake cannot return specified exit code from script,
     # so use FATAL_ERROR for returning non-zero code.
     message(FATAL_ERROR "Validator test '${SUITE}' is failed!")
-elseif(${DIFF_RESULT} EQUAL 2)
-    message(FATAL_ERROR "Diff trouble on test '${SUITE}'")
 endif()
